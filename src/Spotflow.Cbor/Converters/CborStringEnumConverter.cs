17759fea867c941658c66811b061c7242c4dacba
Tomas Pajurek <tomas.pajurek@spotflow.io>
2025-11-26 15:56:29 +0100

    Allow serialization of enums as integers in CborStringEnumConverter

--- src/Spotflow.Cbor/Converters/CborStringEnumConverter.cs
+++ src/Spotflow.Cbor/Converters/CborStringEnumConverter.cs
@@ -1,28 +1,20 @@
 namespace Spotflow.Cbor.Converters;
 
-public class CborStringEnumConverter(bool caseSensitive) : CborConverterFactory
+public class CborStringEnumConverter(bool caseSensitive = false, bool serializeAsInteger = false) : CborConverterFactory
 {
-    public CborStringEnumConverter() : this(caseSensitive: false)
-    {
-    }
-
     public override bool CanConvert(Type typeToConvert) => typeToConvert.IsEnum;
     public override CborConverter CreateConverter(Type typeToConvert, CborSerializerOptions options)
     {
-        return CborEnumConverterFactory.CreateConverter(typeToConvert, serializeAsString: true, allowDeserializationFromString: true, caseSensitive: caseSensitive, options);
+        return CborEnumConverterFactory.CreateConverter(typeToConvert, serializeAsString: !serializeAsInteger, allowDeserializationFromString: true, caseSensitive: caseSensitive, options);
     }
 }
 
-public class CborStringEnumConverter<T>(bool caseSensitive) : CborConverterFactory where T : struct, Enum
+public class CborStringEnumConverter<T>(bool caseSensitive = false, bool serializeAsInteger = false) : CborConverterFactory where T : struct, Enum
 {
-    public CborStringEnumConverter() : this(caseSensitive: false)
-    {
-    }
-
     public override bool CanConvert(Type typeToConvert) => typeToConvert == typeof(T);
     public override CborConverter CreateConverter(Type typeToConvert, CborSerializerOptions options)
     {
-        return CborEnumConverterFactory.CreateConverter(typeToConvert, serializeAsString: true, allowDeserializationFromString: true, caseSensitive: caseSensitive, options);
+        return CborEnumConverterFactory.CreateConverter(typeToConvert, serializeAsString: !serializeAsInteger, allowDeserializationFromString: true, caseSensitive: caseSensitive, options);
     }
 }
 
